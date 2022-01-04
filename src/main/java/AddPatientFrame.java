40b303d7e62a40205ee6073add972bb07375b2ea
Andreea Cojocea <andreeacojocea26@yahoo.com>
2022-01-04 09:57:27 +0000

    Update AddPatientFrame.java
    AddPatientFrame java code

--- src/main/java/AddPatientFrame.java
+++ src/main/java/AddPatientFrame.java
@@ -1,2 +1,32 @@
-public class AddPatientFrame {
+import javax.swing.*;
+
+public class AddPatientFrame extends JFrame{
+    private JButton emergencyButton;
+    private JButton wardButton;
+    private JButton reportButton;
+    private JScrollBar scrollBar1;
+    private JTextField tfFirstName;
+    private JTextField tfLastName;
+    private JTextField tfEmail;
+    private JTextField tfAge;
+    private JButton createProfileButton;
+    private JButton closeButton;
+    private JTextField tfPhoneNumber;
+    private JTextField tfHeight;
+    private JTextField tfWeight;
+    private JTextField tfBloodType;
+    private JTextField tfHospitalized;
+    private JPanel addPatientPanel;
+
+    public AddPatientFrame() {
+        setContentPane(addPatientPanel);
+        setTitle("Add Patient");
+        setSize(450,300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        AddPatientFrame newPatientFrame = new AddPatientFrame();
+    }
 }
