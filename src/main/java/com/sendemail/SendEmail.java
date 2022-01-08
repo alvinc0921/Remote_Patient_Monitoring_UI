06d375537edcc1618ef26940487fa8bad57d1af1
Sam-Oliveira <sdo19@ic.ac.uk>
2022-01-08 14:06:27 +0000

    SendEmail Class added

---
+++ src/main/java/com/sendemail/SendEmail.java
@@ -0,0 +1,77 @@
+package com.sendemail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+
+    public static void main(String[] args) {
+
+        // CODE FROM https://netcorecloud.com/tutorials/send-email-in-java-using-gmail-smtp/
+
+        // Recipient's email ID needs to be mentioned.
+        String to = "sam.oliveira.1331@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "rpmimperial@gmail.com";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("rpmimperial@gmail.com", "vfcvzcljlhxlkwrz");
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("REMOTE PATIENT MONITORING");
+
+            // Now set the actual message
+            message.setText("A PATIENT IS DYING BITCH");
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+    }
+
+}
