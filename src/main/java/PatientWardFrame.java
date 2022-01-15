d0b9ffb4a16c93384880b40ee193f5d57ed0f3b4
dorltcheng <tin.cheng19@imperial.ac.uk>
2022-01-15 14:03:16 +0000

    realtime display of emergency on Ward

--- src/main/java/PatientWardFrame.java
+++ src/main/java/PatientWardFrame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.TimerTask;
 
 public class PatientWardFrame extends JFrame{
     private JTextField tfName1;
@@ -49,7 +50,17 @@ public class PatientWardFrame extends JFrame{
         tfWeight1.setText("67 kg");
         tfBloodType1.setText(pat1.bloodType);
         tfHospitalized1.setText("Floor " + pat1.location.get(0) + ", Room " + pat1.location.get(1) + ", Bed " + pat1.location.get(2));
-        tfEmergency1.setText("none");
+
+        TimerTask readStatus1 = new TimerTask() {   // TimerTask to read the alertStatus of patient every second
+            @Override
+            public void run() {
+                tfEmergency1.setText(pat1.alertStatus);
+            }
+        };
+        java.util.Timer emTimer1 = new java.util.Timer();
+        emTimer1.schedule(readStatus1, 0, 1000);
+
+
 
         tfName2.setText(pat2.firstname + " " + pat2.lastname);
         tfSex2.setText("m");
@@ -58,7 +69,15 @@ public class PatientWardFrame extends JFrame{
         tfWeight2.setText("71 kg");
         tfBloodType2.setText(pat2.bloodType);
         tfHospitalized2.setText("Floor " + pat2.location.get(0) + ", Room " + pat2.location.get(1) + ", Bed " + pat2.location.get(2));
-        tfEmergency2.setText("none");
+
+        TimerTask readStatus2 = new TimerTask() {
+            @Override
+            public void run() {
+                tfEmergency2.setText(pat2.alertStatus);
+            }
+        };
+        java.util.Timer emTimer2 = new java.util.Timer();
+        emTimer2.schedule(readStatus2, 0, 1000);
 
         /*
         tfName3.setText(pat3.firstname + " " + pat3.lastname);
@@ -70,6 +89,16 @@ public class PatientWardFrame extends JFrame{
         tfHospitalized3.setText("Floor " + pat3.location.get(0) + ", Room " + pat3.location.get(1) + ", Bed " + pat3.location.get(2));
         tfEmergency3.setText("none");
 
+        TimerTask readStatus3 = new TimerTask() {   // TimerTask to read the alertStatus of patient every second
+            @Override
+            public void run() {
+                tfEmergency3.setText(pat3.alertStatus);
+            }
+        };
+        java.util.Timer emTimer3 = new java.util.Timer();
+        emTimer3.schedule(readStatus3, 0, 1000);
+
+
         tfName4.setText(pat4.firstname + " " + pat4.lastname);
         tfSex4.setText("f");
         tfAge4.setText(String.valueOf(pat4.age));
@@ -78,6 +107,15 @@ public class PatientWardFrame extends JFrame{
         tfBloodType4.setText(pat4.bloodType);
         tfHospitalized4.setText("Floor " + pat4.location.get(0) + ", Room " + pat4.location.get(1) + ", Bed " + pat4.location.get(2));
         tfEmergency4.setText("none");
+
+        TimerTask readStatus4 = new TimerTask() {   // TimerTask to read the alertStatus of patient every second
+            @Override
+            public void run() {
+                tfEmergency4.setText(pat4.alertStatus);
+            }
+        };
+        java.util.Timer emTimer4 = new java.util.Timer();
+        emTimer4.schedule(readStatus4, 0, 1000);
     */
 
         seePatientDetailsButton1.addActionListener(new ActionListener() {
