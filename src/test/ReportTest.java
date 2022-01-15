79c41d1d921aa87ca47c444fc41744059daceb06
dorltcheng <tin.cheng19@imperial.ac.uk>
2022-01-15 12:38:29 +0000

    ReportTest bug fix

--- src/test/ReportTest.java
+++ src/test/ReportTest.java
@@ -3,11 +3,10 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.lang.*;
 import java.nio.file.*;
 import org.junit.jupiter.api.Test;
@@ -23,15 +22,15 @@ public class ReportTest{
         List<BigDecimal> rrSig = new ArrayList<>();
         List<BigDecimal> ecgSig = new ArrayList<>();
         Patient pat = new Patient(12345,"Alvin","Chen",20,"A+",location,ecgSig,bpSig,hrSig,rrSig,tempSig);
-        generate_report(pat);
+        Reportmenu.generate_report(pat);
         boolean flag1 = false;
         Calendar calendar = Calendar.getInstance();
         Date date = calendar.getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         String current_time = sdf.format(date);
-        String file_path = System.getProperty("user.home") + System.getProperty("file.separator") + "RPM_DATA" + System.getProperty("file.separator") + "Reports" + System.getProperty("file.separator") + pat.getLastname() + "_" + pat.getFirstname() + "_" + pat.getPatID() + System.getProperty("file.separator") + current_pat.getLastname() + "_" + current_pat.getFirstname() + "_" + current_pat.getPatID() + "_" + current_time + ".txt";
+        String file_path = System.getProperty("user.home") + System.getProperty("file.separator") + "RPM_DATA" + System.getProperty("file.separator") + "Reports" + System.getProperty("file.separator") + pat.getLastname() + "_" + pat.getFirstname() + "_" + pat.getPatID() + System.getProperty("file.separator") + pat.getLastname() + "_" + pat.getFirstname() + "_" + pat.getPatID() + "_" + current_time + ".txt";
         Path path = Paths.get(file_path);
         if (Files.exists(path)) flag1=true;
         assertEquals(true,flag1);
     }
-    }
+}
